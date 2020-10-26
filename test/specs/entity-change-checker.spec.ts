a12e0148e33d76c9ee806203c2e28d144a50680c
Moe Myat Zaw <mmzliveid@gmail.com>
2020-10-26 15:59:45 +0630

    Update tests

--- test/specs/entity-change-checker.spec.ts
+++ test/specs/entity-change-checker.spec.ts
@@ -13,19 +13,158 @@ describe('EntityChangeChecker', () => {
         entityChangeChecker = TestBed.inject<EntityChangeChecker>(EntityChangeChecker);
     });
 
-    it('should be dirty', () => {
-        const sourceObj = {
+    it('should NOT be dirty when object values are the same - { dirtyOnly: true, emptyAndNullAsEqual: true }', () => {
+        const sourceObj: Record<string, unknown> = {
             prop1: 'hello',
-            prop2: 'world'
+            prop2: 1,
+            prop3: true,
+            prop4: ['a', 'b'],
+            prop5: [],
+            prop6: { a: 1, b: '2' },
+            prop7: ''
         };
 
+        const modObj: Record<string, unknown> = {
+            prop1: 'hello',
+            prop2: 1,
+            prop3: true,
+            prop4: ['a', 'b'],
+            prop5: [],
+            prop6: { a: 1, b: '2' },
+            prop7: null
+        };
+
+        const isDirty = entityChangeChecker.checkChanges(modObj, sourceObj, {
+            dirtyOnly: true,
+            emptyAndNullAsEqual: true
+        });
+
+        void expect(isDirty).toBeFalsy();
+    });
+
+    it('should be dirty when second object is null - { dirtyOnly: true }', () => {
         const modObj = {
             prop1: 'my',
             prop2: 'world'
         };
 
-        const isDirty = entityChangeChecker.checkChanges(modObj, sourceObj, true);
+        const isDirty = entityChangeChecker.checkChanges(modObj, (null as unknown) as Record<string, unknown>, {
+            dirtyOnly: true
+        });
 
         void expect(isDirty).toBeTruthy();
+    });
+
+    it('should be dirty when simple values are not the same - string - { dirtyOnly: true }', () => {
+        const sourceObj = {
+            prop1: 'hello'
+        };
+
+        const modObj = {
+            prop1: 'my'
+        };
+
+        const isDirty = entityChangeChecker.checkChanges(modObj, sourceObj, { dirtyOnly: true });
+
+        void expect(isDirty).toBeTruthy();
+    });
+
+    it('should NOT be dirty when simple values are the same - string - { dirtyOnly: true }', () => {
+        const sourceObj = {
+            prop1: 'hello'
+        };
+
+        const modObj = {
+            prop1: 'hello'
+        };
+
+        const isDirty = entityChangeChecker.checkChanges(modObj, sourceObj, { dirtyOnly: true });
+
+        void expect(isDirty).toBeFalsy();
+    });
+
+    it('should be dirty when simple values are not the same - boolean - { dirtyOnly: true }', () => {
+        const sourceObj = {
+            prop1: true
+        };
+
+        const modObj = {
+            prop1: false
+        };
+
+        const isDirty = entityChangeChecker.checkChanges(modObj, sourceObj, { dirtyOnly: true });
+
+        void expect(isDirty).toBeTruthy();
+    });
+
+    it('should NOT be dirty when simple values are the same - boolean - { dirtyOnly: true }', () => {
+        const sourceObj = {
+            prop1: true
+        };
+
+        const modObj = {
+            prop1: true
+        };
+
+        const isDirty = entityChangeChecker.checkChanges(modObj, sourceObj, { dirtyOnly: true });
+
+        void expect(isDirty).toBeFalsy();
+    });
+
+    it('should be dirty when simple values are not the same - number - { dirtyOnly: true }', () => {
+        const sourceObj = {
+            prop1: 2
+        };
+
+        const modObj = {
+            prop1: 3
+        };
+
+        const isDirty = entityChangeChecker.checkChanges(modObj, sourceObj, { dirtyOnly: true });
+
+        void expect(isDirty).toBeTruthy();
+    });
+
+    it('should NOT be dirty when simple values are the same - number - { dirtyOnly: true }', () => {
+        const sourceObj = {
+            prop1: 2
+        };
+
+        const modObj = {
+            prop1: 2
+        };
+
+        const isDirty = entityChangeChecker.checkChanges(modObj, sourceObj, { dirtyOnly: true });
+
+        void expect(isDirty).toBeFalsy();
+    });
+
+    it('should be dirty when simple values are not the same - Date - { dirtyOnly: true }', () => {
+        const sourceObj = {
+            prop1: new Date('20201010')
+        };
+
+        const modObj = {
+            prop1: new Date()
+        };
+
+        const isDirty = entityChangeChecker.checkChanges(modObj, sourceObj, { dirtyOnly: true });
+
+        void expect(isDirty).toBeTruthy();
+    });
+
+    it('should NOT be dirty when simple values are the same - Date - { dirtyOnly: true }', () => {
+        const d = new Date();
+        const sourceObj = {
+            prop1: d
+        };
+
+        const modObj = {
+            prop1: d
+        };
+
+        const isDirty = entityChangeChecker.checkChanges(modObj, sourceObj, { dirtyOnly: true });
+
+        void expect(isDirty).toBeFalsy();
     });
 });
